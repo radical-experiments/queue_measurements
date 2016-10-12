500f731c23046bf34b490060394489682317b894
mingtaiha <ming.tai.ha@gmail.com>
2016-10-12 16:45:28 +0200

    Started Collecting Data

--- queue_probe.py
+++ queue_probe.py
@@ -1,25 +1,26 @@
 import os
+import sys
 import json
 import random
 import radical.pilot as rp
-import karnak_reader as kr
 
-SAMPLE_SIZE = 30
-wALLTIME = 30   #In minutes
+SAMPLE_SIZE = 20
+WALLTIME = 30   #In minutes
+OVERHEAD = 3    #Overhead incurred throughout the process
 CORES = 16
 UTIL = 1        #Percent of Cores Utilized
 
 def job_select():
     pass    
 
-if sys.argv[1] == "stampede" or sys.argv[1] == "gordon" or sys.argv[1] == "comet" or sys.argv[1] == supermic:
+if sys.argv[1] == "stampede" or sys.argv[1] == "gordon" or sys.argv[1] == "comet" or sys.argv[1] == "supermic":
     res = sys.argv[1]
     random.seed()
 else:
     sys.exit()
 
 
-for run in range():
+for run in range(SAMPLE_SIZE):
 
     with open('resource_handle.json') as j:
         conf = json.load(j)
@@ -28,14 +29,15 @@ for run in range():
 
     try:    
         session = rp.Session()
+        session_id = session.uid
         pmgr = rp.PilotManager(session=session)
-        pdesc = list()
+        pdescs = list()
     
         pd_init = { 'resource'      :   res_conf['name'],
                     'queue'         :   res_conf['queue'],
                     'access_schema' :   res_conf['access_schema'],
-                    'cores'         :   CORE_SIZE,
-                    'runtime'       :   wALLTIME,
+                    'cores'         :   CORES,
+                    'runtime'       :   WALLTIME,
                     'project'       :   'TG-MCB090174'
                   }
         pdescs.append(rp.ComputePilotDescription(pd_init))
@@ -46,10 +48,10 @@ for run in range():
     
         cuds = list()
     
-        for task in range(CORE_SIZE * UTIL):
+        for task in range(CORES * UTIL):
             cud = rp.ComputeUnitDescription()
             cud.executable = '/bin/sleep'
-            cud.arguments = [RUN_TIME]
+            cud.arguments = [(WALLTIME - OVERHEAD) * 60] #/bin/sleep assumes input argument in sec
             cuds.append(cud)
     
         umgr.submit_units(cuds)
@@ -58,10 +60,10 @@ for run in range():
     except Exception as e:
         print "Caught Exception %s\n" % e
 
-    except(KeyboardInterrupt, System) as e:
+    except(KeyboardInterrupt, SystemExit) as e:
         print "Caught A Keyboard Error"
 
     finally:
         os.system("radicalpilot-close-session -m export -s {0}".format(session_id))
-        os.system("mv {0}+.json {1}".format(session_id, resource))
+        os.system("mv {0}.json {1}".format(session_id, res))
         session.close()
---
+++ timings.py
@@ -0,0 +1,65 @@
+import json
+import subprocess
+import sys
+from pprint import pprint
+
+folder = sys.argv[1]
+
+
+def get_timings(session_json):
+    with open(session_json) as j:
+        j_file = json.load(j)
+        
+    state_history = j_file["pilot"][0]["statehistory"]
+
+    pilot_lifetime = dict()
+
+    for state in range(len(state_history)):
+        pilot_lifetime[state_history[state]["state"]] = state_history[state]["timestamp"]
+
+    #for state in pilot_lifetime:
+    #    if state != "PendingLaunch":
+    #        pilot_lifetime[state] -= pilot_lifetime["PendingLaunch"]
+    #pilot_lifetime["PendingLaunch"] = 0.0
+
+    #print pilot_lifetime
+
+    if "Active" in pilot_lifetime and "PendingActive" in pilot_lifetime:
+        Tq = pilot_lifetime["Active"] - pilot_lifetime["PendingActive"]
+    else:
+        print "There was no Tq"
+        Tq = -1
+
+    if "Active" in pilot_lifetime:
+        if "Canceled" in pilot_lifetime:
+            Tx = pilot_lifetime["Canceled"] - pilot_lifetime["Active"]
+        elif "Done" in pilot_lifetime:
+            Tx = pilot_lifetime["Done"] - pilot_lifetime["Active"]
+        else:
+            unit_state_history = [j_file["unit"][i]["statehistory"][-1] for i in range(len(j_file["unit"]))]
+            #pprint(unit_state_history)
+            num_units_finished = j_file["pilot"][0]["description"]["cores"]
+            if num_units_finished == len(unit_state_history):
+                done_timings = [unit_state_history[i]["timestamp"] for i in range(num_units_finished)]
+                #print done_timings
+                max_done_timestamp = max(done_timings)
+                #print max_done_timestamp
+                Tx = max_done_timestamp - pilot_lifetime["Active"]
+            else:
+                print "The pilot entered 'Failed' state, or did not reach a final state"
+                Tx = -1
+    else:
+        print "The pilot entere 'Failed' state before becoming Active"
+        Tx = -1
+
+    print "Session: {0}, Tx: {1}, Tq: {2}".format(session_json, Tx, Tq)
+
+def get_unit_timings(session_json):
+    pass
+
+proc = subprocess.Popen(['ls ' + folder], stdout=subprocess.PIPE, shell=True)
+(session_list, err) = proc.communicate()
+session_list = session_list.split('\n')
+del session_list[-1]
+for session in session_list:
+    get_timings(folder+"/"+session)
